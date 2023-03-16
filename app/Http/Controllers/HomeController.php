4915bfd7c326135465b0dc9c7781d51167fe775b
milandhadukit <milandhaduk.siliconithub@gmail.com>
2023-03-16 11:45:34 +0530

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
     public function index()
     {
        
-        //view page  
+        //view page  fgfddfgdfg
         return view('home');
     }
     
